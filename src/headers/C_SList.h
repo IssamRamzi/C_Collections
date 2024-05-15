03fd1dfae0d40e9ab9d61840585dd6d488da9dff
Ramzi <iss.benahmed@outlook.com>
2024-05-15 21:54:36 +0200

    Added Functions Declaration

--- src/headers/C_SList.h
+++ src/headers/C_SList.h
@@ -23,6 +23,25 @@ enum C_COMMON   C_SList_add_all_at(C_SList *list1, C_SList *list2, size_t index)
 enum C_COMMON   C_SList_add_Last(C_SList *list,void *data);
 
 
+enum C_COMMON   C_SList_remove_First(C_SList *list);
+enum C_COMMON   C_SList_remove_Last(C_SList *list);
+enum C_COMMON   C_SList_remove_at(C_SList *list);
+enum C_COMMON   C_SList_remove_value(C_SList *list, void *data);
+enum C_COMMON   C_SList_remove_all(C_SList *list, C_SList *list2);
+enum C_COMMON   C_SList_remove_all_at(C_SList *list, C_SList *list2, size_t index);
+
+
+void *          C_SList_get_at(C_SList *list, size_t index);
+
+
+size_t          C_SList_contains(C_SList *list, void *data);
+size_t          C_SList_contains_all(C_SList *list,C_SList *list2);
+
+
+void            C_SList_reverse(C_SList *list);
+// void            C_SList_sort(C_SList *list);
+
+
 
 
 
