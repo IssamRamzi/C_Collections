1b702f3910cf8b769158501d59a5d19f8d301887
Ramzi <iss.benahmed@outlook.com>
2024-05-15 21:45:33 +0200

    Added Run on windows

--- src/main.c
+++ src/main.c
@@ -3,8 +3,20 @@
 
 int main(void)
 {
-    printf("Hello World");
-    C_SList *list = create_C_SList();
+    C_SList *list = C_SList_create();
+    C_SList_add_First(list,"Hello World");
+    C_SList_add_Last(list,"Salem");
+    C_SList_add_Last(list,"Bonjour");
+    C_SList_add_Last(list,"Hola");
+    C_SList_add_Last(list,"Shanghai");
+    C_SList_add_Last(list,"JAI DES PULSIONS BIZARRES C UN MYSTERE");
+    C_SList_add_Last(list,5);
+    printf("%s\n",list->head->data);
+    printf("%d",list->tail->data);
+
+
+    
+    
     
 
     printf("\n");
